7cd5fd4d34cd564a199c088f5c0b83750b9a77e6
woozxn <squib0612@gmail.com>
2022-08-29 21:52:34 +0900

    Initial commit

---
+++ src/main/java/com/univnote/UnivnoteApplication.java
@@ -0,0 +1,13 @@
+package com.univnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univnote/UnivnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
